a94a189dc0b8c3065d8cb2508da6c7b418c0004b
Paola <paolagrillo080101@gmail.com>
2026-06-14 11:18:59 +0200

    lista nemici

---
+++ script.js
@@ -0,0 +1,43 @@
+let lista = document.getElementById("lista");
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let cancella = document.getElementById("cancella");
+let modalitaElimina = false;
+let nomi = JSON.parse(localStorage.getItem("nomi")) || [];
+
+function creaNome(nome) {
+    let p = document.createElement("p");
+    p.textContent = nome;
+
+    let x = document.createElement("button");
+    x.textContent = "X";
+    x.style.display = "none";
+    x.addEventListener("click", function() {
+        p.remove();
+        nomi = nomi.filter(function(n) { return n !== nome; });
+        localStorage.setItem("nomi", JSON.stringify(nomi));
+    });
+
+    p.appendChild(x);
+    lista.appendChild(p);
+}
+
+nomi.forEach(function(nome) {
+    creaNome(nome);
+});
+
+btn.addEventListener("click", function() {
+    let nome = input.value;
+    nomi.push(nome);
+    localStorage.setItem("nomi", JSON.stringify(nomi));
+    creaNome(nome);
+    input.value = "";
+});
+
+cancella.addEventListener("click", function() {
+    modalitaElimina = !modalitaElimina;
+    let bottoniX = document.querySelectorAll("#lista button");
+    bottoniX.forEach(function(x) {
+        x.style.display = modalitaElimina ? "inline" : "none";
+    });
+});
